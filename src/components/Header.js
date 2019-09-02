2085248a8bba1b9e9b270f6835c6733dc77ff02a
Negar Rahbar <neg.rahbar@gmail.com>
2019-09-02 16:12:57 +0200

    Change the name and title

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
 let data = {
-  name: 'Rahul Bhatia',
-  role: 'Frontend Developer and Data Scientist',
+  name: 'Negar Rahbar',
+  role: 'Mathematics Tutor',
   roleDescription:
     'I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.',
   socialLinks: [
